d97be08a300aa84bef7db513a10b818110cb65e3
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-10-04 17:46:58 -0500

    Creación clase Card

---
+++ ClassLibrary/Card.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Card
+    {
+        // atributos de clase carta
+        private char suit;
+        private string symbol;
+        private int score;
+        private string color;
+
+        public char Suit { get => suit; set => suit = value; }
+        public string Symbol { get => symbol; set => symbol = value; }
+        public int Score { get => score; set => score = value; }
+        public string Color { get => color; set => color = value; }
+
+        public Card(char suit, string symbol)
+        {
+            this.suit = suit;
+            this.symbol = symbol;
+
+            if (suit == '♦' || suit == '♥')
+            {
+                this.color = "red";
+
+            }
+            else
+            {
+                this.color = "black";
+            }
+
+            int value;
+            try
+            {
+                value = Convert.ToInt32(symbol);
+            }
+            catch
+            {
+                if (symbol == "A")
+                {
+                    value = 1;
+                }
+                else
+                {
+                    value = 10;
+                }
+            }
+            this.score = value;
+
+        }
+    }
+}
+
