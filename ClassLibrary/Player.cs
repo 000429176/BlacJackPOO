4172c4a1ea03d4137089ff7b02803ebb03ea9885
SilviaPabon <silvia.pabon.2020@users.noreply.github.com>
2020-10-04 18:13:00 -0500

    Creación clase Player

--- ClassLibrary/Player.cs
+++ ClassLibrary/Player.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace ClassLibrary
 {
-    public class Player
+    public class Player : User
     {
+        public void Init(Card c1, Card c2)
+        {
+            Hand = new List<Card>();
+            AddCard(c1);
+            AddCard(c2);
+        }
     }
 }
