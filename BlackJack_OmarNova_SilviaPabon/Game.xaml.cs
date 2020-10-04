d6a341deadb42f58b548828e78a0376da67b5fe2
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-10-04 17:27:25 -0500

    Inicio del laboratorio. Creación de ventanas.

---
+++ BlackJack_OmarNova_SilviaPabon/Game.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BlackJack_OmarNova_SilviaPabon
+{
+    /// <summary>
+    /// Lógica de interacción para Game.xaml
+    /// </summary>
+    public partial class Game : Window
+    {
+        public Game()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
